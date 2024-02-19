95f7d7b1a9a7c1a811f7fd01b434b0f2dda9f461
Nikola Berankova <nikoneecha@gmail.com>
2024-02-19 20:15:45 +0100

    feat: add category check

--- server/src/domains/recipe/services/recipe.services.ts
+++ server/src/domains/recipe/services/recipe.services.ts
@@ -1,6 +1,7 @@
 import { RecipeRepository } from '../repositories/recipe.repository.interface'
 import { RecipeSchema } from '../schemas/recipe.schema'
 import { NotFoundError } from '../../../lib/errors'
+import { UnprocessableEntityError } from '../../../lib/errors'
 
 export const recipeService = (recipeRepository: RecipeRepository) => {
   const getAllRecipes = async () => {
@@ -16,7 +17,11 @@ export const recipeService = (recipeRepository: RecipeRepository) => {
   }
 
   const createRecipe = async (recipe: RecipeSchema) => {
-    return await recipeRepository.createRecipe(recipe)
+    const createdRecipe = await recipeRepository.createRecipe(recipe)
+    if (!createdRecipe.categoryId) {
+      throw new UnprocessableEntityError('Missing category')
+    }
+    return createdRecipe
   }
 
   const updateRecipe = async (id: string, recipe: RecipeSchema) => {
