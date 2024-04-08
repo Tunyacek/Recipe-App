68efb51432e60ec3328d04a117585ba3be0e4add
Nikola Berankova <nikoneecha@gmail.com>
2024-04-08 15:31:10 +0200

    feat: add multer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from 'express'
 import { errorHandler } from './middleware'
 import { recipeModule } from './domains/recipe/recipe.module'
+import { imageModule } from './domains/image/image.module'
 import 'dotenv/config'
 import cors from 'cors'
+import multer from 'multer'
+
+const upload = multer({ dest: 'uploads/' })
 
 const app: Express = express()
 const port = process.env.PORT || 3000
 
 const recipeAPI = recipeModule()
+
+const imageAPI = imageModule()
 
 app.use(express.json())
 app.use(cors({ origin: process.env.FE_URL }))
