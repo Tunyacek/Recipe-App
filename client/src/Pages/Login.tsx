4b777663b7012c80aa3bc49ce9f55e8902440ae9
Nikola Berankova <nikoneecha@gmail.com>
2024-08-15 19:15:13 +0200

    feat: add errors to registration, implement database connection

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,11 +1,23 @@
-import { Box, Text, Input, Flex, FormLabel } from '@chakra-ui/react'
+import {
+  Box,
+  Text,
+  Input,
+  Flex,
+  FormLabel,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from '@chakra-ui/react'
 import { HeaderLogo } from '../Components/Shared/Header/Header'
 import { Footer } from '../Components/Shared/Footer/Footer'
-import { PasswordInput } from '../Components/Login_and_Register/PasswordInput'
 import { BackButton, LoginSubmit } from '../Components/Shared/Buttons/Button'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export function LoginForm() {
+  const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show)
+
   return (
     <Box bg="#d0ffd5">
       <Box ml="5px">
@@ -16,7 +28,14 @@ export function LoginForm() {
           <BackButton />
         </Link>
       </Box>
-      <Box minHeight="75.7vh" bg="#f3fff4">
+      <Box
+        minHeight="75.7vh"
+        bg="#f3fff4"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
         <Flex>
           <Text as="b" fontSize="30px" m="20px">
             Přihlášení
@@ -24,18 +43,36 @@ export function LoginForm() {
         </Flex>
         <form>
           <Flex direction="column" gap="3" m="20px">
-            <FormLabel>Emailová adresa</FormLabel>
-            <Input
-              type="email"
-              width="400px"
-              borderColor="#9acc9c"
-              bg="white"
-              focusBorderColor="#9acc9c"
-            />
-            <FormLabel>Heslo</FormLabel>
-            <PasswordInput />
+            <Box>
+              <FormLabel>Uživatelské jméno</FormLabel>
+              <Input width="400px" borderColor="#9acc9c" bg="white" focusBorderColor="#9acc9c" />
+            </Box>
+            <Box>
+              <FormLabel>Heslo</FormLabel>
+              <InputGroup size="md" width="400px">
+                <Input
+                  pr="4.5rem"
+                  type={show ? 'text' : 'password'}
+                  borderColor="#9acc9c"
+                  focusBorderColor="#9acc9c"
+                  bg="white"
+                />
+                <InputRightElement width="6rem" pr="8px">
+                  <Button
+                    h="1.75rem"
+                    width="100px"
+                    size="xl"
+                    onClick={handleClick}
+                    bg="#9acc9c"
+                    _hover={{ background: '#8cb88d' }}
+                  >
+                    {show ? 'Skrýt' : 'Zobrazit'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </Box>
           </Flex>
-          <Box ml="350px" mt="30px">
+          <Box ml="175px" mt="30px">
             <LoginSubmit />
           </Box>
         </form>
--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -1,11 +1,72 @@
-import { Box, Flex, FormLabel, Input, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useToast,
+} from '@chakra-ui/react'
 import { HeaderLogo } from '../Components/Shared/Header/Header'
 import { Footer } from '../Components/Shared/Footer/Footer'
-import { PasswordInput } from '../Components/Login_and_Register/PasswordInput'
 import { BackButton, RegisterSubmit } from '../Components/Shared/Buttons/Button'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
+import { type FormEvent, useState } from 'react'
+import axios from 'axios'
+
+const url = import.meta.env.VITE_BE_URL
+
+const THREE_THOUSAND = 3000
 
 export function RegisterForm() {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [redirect, setRedirect] = useState(false)
+  const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show)
+  const toast = useToast()
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    if (passwordConfirm !== password) {
+      toast({
+        title: 'Hesla se neshodují',
+        description: 'Zkontrolujte, zda se vaše hesla shodují.',
+        status: 'error',
+        duration: THREE_THOUSAND,
+        isClosable: true,
+      })
+      return
+    }
+
+    try {
+      await axios.post(`${url}/register`, {
+        email,
+        username,
+        password,
+      })
+
+      setRedirect(true)
+    } catch (error) {
+      console.error('Error occurred during registration:', error)
+      toast({
+        title: 'ZKONTROLUJ DATABAZI NA EMAIL A USERNAME A NAPIŠ ERROR',
+        description: 'Nastala chyba při registraci. Zkuste to prosím znovu.',
+        status: 'error',
+        duration: THREE_THOUSAND,
+        isClosable: true,
+      })
+    }
+  }
+  if (redirect) {
+    return <Navigate to="/login" />
+  }
+
   return (
     <Box bg="#d0ffd5">
       <Box ml="5px">
@@ -16,27 +77,97 @@ export function RegisterForm() {
           <BackButton />
         </Link>
       </Box>
-      <Box minHeight="75.7vh" bg="#f3fff4">
+      <Box
+        minHeight="75.7vh"
+        bg="#f3fff4"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
         <Flex>
           <Text as="b" fontSize="30px" m="20px">
             Registrace
           </Text>
         </Flex>
-        <Flex direction="column" gap="3" m="20px">
-          <FormLabel>Emailová adresa</FormLabel>
-          <Input
-            type="email"
-            borderColor="#9acc9c"
-            width="400px"
-            bg="white"
-            focusBorderColor="#9acc9c"
-          />
-          <FormLabel>Heslo</FormLabel>
-          <PasswordInput />
-        </Flex>
-        <Box ml="350px" mt="30px">
-          <RegisterSubmit />
-        </Box>
+        <form onSubmit={handleSubmit}>
+          <Flex direction="column" gap="3" m="20px">
+            <Box>
+              <FormLabel>Emailová adresa</FormLabel>
+              <Input
+                type="email"
+                borderColor="#9acc9c"
+                width="400px"
+                bg="white"
+                focusBorderColor="#9acc9c"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Box>
+            <Box>
+              <FormLabel>Uživatelské jméno</FormLabel>
+              <Input
+                borderColor="#9acc9c"
+                width="400px"
+                bg="white"
+                focusBorderColor="#9acc9c"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Box>
+            <Box>
+              <FormLabel>Heslo</FormLabel>
+              <InputGroup size="md" width="400px">
+                <Input
+                  pr="4.5rem"
+                  type={show ? 'text' : 'password'}
+                  borderColor="#9acc9c"
+                  focusBorderColor="#9acc9c"
+                  bg="white"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement width="6rem" pr="8px">
+                  <Button
+                    h="1.75rem"
+                    width="100px"
+                    size="xl"
+                    onClick={handleClick}
+                    bg="#9acc9c"
+                    _hover={{ background: '#8cb88d' }}
+                  >
+                    {show ? 'Skrýt' : 'Zobrazit'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </Box>
+            <Box>
+              <FormLabel>Kontrola hesla</FormLabel>
+              <InputGroup size="md" width="400px">
+                <Input
+                  pr="4.5rem"
+                  type={show ? 'text' : 'password'}
+                  borderColor="#9acc9c"
+                  focusBorderColor="#9acc9c"
+                  bg="white"
+                  onChange={(e) => setPasswordConfirm(e.target.value)}
+                />
+                <InputRightElement width="6rem" pr="8px">
+                  <Button
+                    h="1.75rem"
+                    width="100px"
+                    size="xl"
+                    onClick={handleClick}
+                    bg="#9acc9c"
+                    _hover={{ background: '#8cb88d' }}
+                  >
+                    {show ? 'Skrýt' : 'Zobrazit'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </Box>
+          </Flex>
+          <Box ml="160px" mt="30px">
+            <RegisterSubmit />
+          </Box>
+        </form>
       </Box>
       <Footer />
     </Box>
