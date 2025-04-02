bd6f4de67f46446dd57c643daec2f0e96eef7d10
Nikola Berankova <nikoneecha@gmail.com>
2025-04-02 15:43:15 +0200

    chore: better responsiveness of card, category dropdown and searchbar

--- client/src/Components/Homepage/RecipeList.tsx
+++ client/src/Components/Homepage/RecipeList.tsx
@@ -63,7 +63,6 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
 
           '@media screen and (min-width: 515px) and (max-width: 699px)': {
             maxW: '600px',
-            ml: '30px',
           },
           '@media screen and (max-width: 509px)': {
             ml: '30px',
